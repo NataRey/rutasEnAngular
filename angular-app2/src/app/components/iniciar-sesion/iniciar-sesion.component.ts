a520f95f63f6aa70c0b36482b7f03a6a52b052ff
NataRey <lenarero@gmail.com>
2024-05-18 22:13:26 -0500

    llevando datos del incio de sesion

--- angular-app2/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
+++ angular-app2/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+//importo mis dependecias para el formulario
+import { FormsModule, ReactiveFormsModule, FormGroup,FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
-  imports: [],
+  //activo mis importaciones
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
 export class IniciarSesionComponent {
-
+  Formulariocredenciales = new FormGroup({
+    usuario: new FormControl(''),
+    contrasenia: new FormControl('')
+  });
+  manejarEnvio(){
+    console.log("credenciales", this.Formulariocredenciales.value);
+  }
 }
--- angular-app2/src/app/components/navegacion/navegacion.component.ts
+++ angular-app2/src/app/components/navegacion/navegacion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { router}
 
 @Component({
   selector: 'app-navegacion',
